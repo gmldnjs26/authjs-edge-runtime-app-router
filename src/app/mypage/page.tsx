b390a8670d4d1b7fae7a3a2e729e4238a43f2aeb
gmldnjs26 <imleesky265@gmail.com>
2024-03-23 19:01:59 +0900

    fix

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -4,5 +4,10 @@ import { useSession } from "next-auth/react";
 
 export default function MyPage() {
   const { data: session, status } = useSession();
-  return <div>MyPage</div>;
+  return (
+    <div>
+      MyPage
+      {status === "authenticated" && <div>{session.user?.email}</div>}
+    </div>
+  );
 }
